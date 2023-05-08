84306839c7e724c421042bd1d60fac6e156d8df2
hnm938 <mrbooomboom@gmail.com>
2023-05-08 19:04:51 -0400

    Added advanced password options

--- script.js
+++ script.js
@@ -6,23 +6,54 @@ var enableSymbols = false;
 var enableUppercase = false;
 var enableNumbers = false;
 
+// variables for number advanced options
+var numberRangeMin = 1;
+var numberRangeMax = 3;
+var numberRangeIncrease = 1;
+var numberRangeStep = 4;
+
+// variables for uppercase advanced options
+var uppercaseRangeMin = 1;
+var uppercaseRangeMax = 3;
+var uppercaseRangeIncrease = 1;
+var uppercaseRangeStep = 2;
+
+// variables for symbol advanced options
+var symbolRangeMin = 1;
+var symbolRangeMax = 2;
+var symbolRangeIncrease = 1;
+var symbolRangeStep = 6;
+
 function generatePassword(passwordLength) {
   if (passwordLength <= 50 && passwordLength >= 1) {
     // ADD ADVANCED SECTION
     // MAKE RANGES MODIFIABLE
     // MAKE RANGE INCREASE MODIFIABLE
-    
+
     // Number variables
     var numberCount = 0;
-    var numberCountRange = [1, 3 + Math.floor(passwordLength / 4)];
+    var numberCountRange = [
+      numberRangeMin,
+      numberRangeMax +
+        Math.floor(passwordLength / numberRangeStep) * numberRangeIncrease,
+    ];
 
     // Uppercase variables
     var uppercaseCount = 0;
-    var uppercaseCountRange = [1, 3 + Math.floor(passwordLength / 2)];
+    var uppercaseCountRange = [
+      uppercaseRangeMin,
+      uppercaseRangeMax +
+        Math.floor(passwordLength / uppercaseRangeStep) *
+          uppercaseRangeIncrease,
+    ];
 
     // Symbol variables
     var symbolCount = 0;
-    var symbolCountRange = [1, 2 + Math.floor(passwordLength / 6)];
+    var symbolCountRange = [
+      symbolRangeMin,
+      symbolRangeMax +
+        Math.floor(passwordLength / symbolRangeStep) * symbolRangeIncrease,
+    ];
 
     // Output variables
     var generatedPassword = "";
@@ -95,6 +126,27 @@ function copyPassword() {
   navigator.clipboard.writeText(passwordOutput.value);
 
   alert("🗝️ Password Copied to Clipboard 🗝️");
+}
+
+function advancedInfo() {
+  alert(`
+  Ranges:
+  The range of each field is what determins how many 
+  characters will be replaced with the given field.
+
+  This range is picked at random meaning if the symbols range is 
+  set to 1 - 4, a random number from 1 to 4 characters will be 
+  replaced by a symbol.
+
+  Increase and Step:
+  The increase simply increase the range by the given number
+  every step, a Step is however many characters it takes to 
+  increase the range.
+
+  This means if the symbols increase is 1 and step is 6,
+  the allowed number of symbols will increase 1 by
+  every 6 characters.
+  `);
 }
 
 function init() {
